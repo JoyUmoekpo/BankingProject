febb251f410632893fb1d28f460c812ec4d692cc
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-03-26 13:18:34 -0500

    Edited ConnectionUtils

--- src/main/java/com/revature/utilities/ConnectionUtils.java
+++ src/main/java/com/revature/utilities/ConnectionUtils.java
@@ -11,7 +11,7 @@ public class ConnectionUtils {
             // A factory - pass in string details for any type of database.
             // The DriverManager factory will give you back a connection
             // implementation specifically for Postgres.
-            Connection conn = DriverManager.getConnection(url);
+            Connection conn = DriverManager.getConnection(url);            
             return conn;
         } catch (SQLException e) {
             e.printStackTrace();
