4ef23632b92dfa24b6cfaf05a8dca85f12b51140
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-03-27 18:34:46 -0500

    Formatted Client Class

--- src/main/java/com/revature/entities/Clients.java
+++ src/main/java/com/revature/entities/Clients.java
@@ -1,5 +1,41 @@
 package com.revature.entities;
 
 public class Clients {
+	private int id;
+	private String name;
+
+	public Clients() {
+		super();
+	}
+
+	public Clients(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Clients "
+				+ "[id=" + id + 
+				", name=" + name +				
+				"]";
+	}
 
 }
